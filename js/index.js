02bf2a01b9b403cf080c02243e4dd15c7f4e9f3b
Opdata <pb03641@gmail.com>
2026-07-06 11:40:22 +0900

    feat: repo api, nav scroll event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const initNavScroll = () => {
+  const nav = document.querySelectorAll('.navbar a').forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const target = document.querySelector(link.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+};
+
+const getRepository = async () => {
+  const res = await fetch('https://api.github.com/users/opdata/repos');
+
+  if (!res.ok) {
+    throw new Error('프로젝트를 불러올 수 없습니다.');
+  }
+
+  const result = await res.json();
+
+  return result;
+};
+
+getRepository();
+initNavScroll();
